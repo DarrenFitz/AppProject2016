c3bc3e34e03a0f56635939e79779c3669becf219
DarrenFitz <dfitzpatrick12@gmail.com>
2016-04-03 14:33:38 +0100

    camera

--- AppProject2016/AppProject2016/Page3.xaml.cs
+++ AppProject2016/AppProject2016/Page3.xaml.cs
@@ -12,6 +12,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Media.Editing;
+using Windows.Media.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +29,66 @@ namespace AppProject2016
     /// </summary>
     public sealed partial class Page3 : Page
     {
+
         public Page3()
         {
             this.InitializeComponent();
+        }
+
+        private async void BtnCapture_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(300, 300);
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+            SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+            BitmapPixelFormat.Bgra8,
+            BitmapAlphaMode.Premultiplied);
+
+            SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+            imageControl.Source = bitmapSource;
+        }
+
+        private async void BtnImages_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.VideoSettings.Format = CameraCaptureUIVideoFormat.Mp4;
+
+            StorageFile videoFile = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Video);
+
+            if (videoFile == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+
+            MediaComposition mediaComposition;
+            MediaStreamSource mediaStreamSource;
+
+            mediaComposition = new MediaComposition();
+
+            MediaClip mediaClip = await MediaClip.CreateFromFileAsync(videoFile);
+
+            mediaComposition.Clips.Add(mediaClip);
+            mediaStreamSource = mediaComposition.GeneratePreviewMediaStreamSource(
+                (int)mediaElement.ActualWidth,
+                (int)mediaElement.ActualHeight);
+
+            mediaElement.SetMediaStreamSource(mediaStreamSource);
         }
     }
 }
